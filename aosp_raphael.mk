e59ced5a4e6d7e625c778f44de467920ad066329
Rahul Singh <demogorgon@pop-os.localdomain>
2021-10-03 20:33:40 +0530

    AospExtended Initial Setup

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common Aex stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ancient_raphael
+PRODUCT_NAME := aosp_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -27,15 +27,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
-# Ancient stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
 EXTRA_FOD_ANIMATIONS := true
-
-# Ancient
-ANCIENT_OFFICIAL=true
-#TARGET_GAPPS_ARCH := arm64
-#IS_PHONE := true
-#ANCIENT_GAPPS := true
-#ANCIENT_WEEABO := true
